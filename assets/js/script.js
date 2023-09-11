1230f4f46633c5775c38a3ebb7e7d2b18cb4d9ea
urbanykv <matheusurban19@gmail.com>
2023-09-11 18:44:30 -0300

    Atualizações projeto consultar cep

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const inputCep = document.querySelector('#cep');
+const btnConsultar = document.querySelector('#btn');
+const resultadoConsulta = document.querySelector('#resultadoConsulta');
+
+function render(){
+    
+}
