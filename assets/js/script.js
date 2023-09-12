2ec4ef2b7e78b0bdd3841c6b927c7f262854ffe1
urbanykv <matheusurban19@gmail.com>
2023-09-11 21:50:37 -0300

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-const inputCep = document.querySelector('#cep');
+const cep = document.querySelector('#cep').value;
 const btnConsultar = document.querySelector('#btn');
+const btnApagar = document.querySelector('.clear');
 const resultadoConsulta = document.querySelector('#resultadoConsulta');
+const msgInicial = document.querySelector('#msgInicial')
 
-function render(){
-    
-}
+btnApagar.addEventListener('click', function(){
+    cep.value = '';
+});
+
+btnConsultar.addEventListener('click', function consultar(){
+    msgInicial.style.display = 'none'
+
+    let txtEndereco = '';
+    let url = 'https://viacep.com.br/ws/' + cep + '/json/';
+
+    fetch(url).then(function(response){
+        console.log(response);
+    })
+});
